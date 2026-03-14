cc8387f959a500dbaeeef7f91502646739142e98
Munib Ahmed <munibahmed@Munibs-MacBook-Air.local>
2026-03-14 06:42:38 +0500

    First Commit

---
+++ src/components/Contact.tsx
@@ -0,0 +1,68 @@
+import { MdArrowOutward, MdCopyright } from "react-icons/md";
+import "./styles/Contact.css";
+
+const Contact = () => {
+  return (
+    <div className="contact-section section-container" id="contact">
+      <div className="contact-container">
+        <h3>Contact</h3>
+        <div className="contact-flex">
+          <div className="contact-box">
+            <h4>Email</h4>
+            <p>
+              <a href="mailto:munibahmed016@gmail.com" data-cursor="disable">
+                munibahmed016@gmail.com
+              </a>
+            </p>
+            <h4>Education</h4>
+            <p>BSCS in Computer Science</p>
+          </div>
+          <div className="contact-box">
+            <h4>Social</h4>
+            <a
+              href="https://github.com/munibahmed016"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Github <MdArrowOutward />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/munib-ahmed-289aa0207/"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Linkedin <MdArrowOutward />
+            </a>
+            {/* <a
+              href="https://x.com/raxx21_official"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Twitter <MdArrowOutward />
+            </a>
+            <a
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Instagram <MdArrowOutward />
+            </a> */}
+          </div>
+          <div className="contact-box">
+            <h2>
+              Designed and Developed <br /> by <span>Munib Ahmed</span>
+            </h2>
+            <h5>
+              <MdCopyright /> 2026 Munib Ahmed. All rights reserved.
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
